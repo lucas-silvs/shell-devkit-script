77bd4cdb77005a2548aa70165bfcd5e48bf7ce1d
Lucas Santos <lucas.parkl@outlook.com>
2024-07-05 00:48:58 -0300

    feat: atualização script - julho/2024: - tornando instalação do ZSH opcional - adicionando instalação do Chrome

--- atualiza_instala_dependencias.sh
+++ atualiza_instala_dependencias.sh
@@ -40,13 +40,17 @@ sudo snap install intellij-idea-ultimate --classic
 sudo snap install postman
 sudo snap install code --classic
 
+# instalando Chrome
+wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./chrome.deb
+rm chrome.deb   
 
 
 # Instala o sdkman
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# Instala o Java 17, o Gradle, Maven e quarkus mais recente com o sdkman
+# Instala o Java, o Gradle, Maven e quarkus mais recente com o sdkman
 sdk install java
 sdk install gradle
 sdk install maven
@@ -57,20 +61,5 @@ curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /us
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
 sudo apt update
 sudo apt -y install lens
-
-# Baixa e instala as fontes do Powerlevel10k
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-mv *.ttf /home/$USER/.fonts
-fc-cache -f -v
-
-# Altera o tema do terminal para MesloLGS NF Regular
-sed -i 's/font = Monospace 10/font = MesloLGS NF 10/g' ~/.config/alacritty/alacritty.yml
-
-# Altera a fonte do GNOME Terminal para MesloLGS NF Regular
-sed -i 's/monospace/MesloLGS NF Regular/g' ~/.config/gtk-3.0/settings.ini
-
 
 echo "Atualização e instalação de dependências concluídas."
